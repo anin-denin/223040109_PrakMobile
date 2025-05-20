692f6ad20a81f7bcecf2e9f83774f877828ede15
Anin <107341855+anin-denin@users.noreply.github.com>
2025-05-20 20:07:42 +0700

    Add files via upload

---
+++ ppm04/models/Note.kt
@@ -0,0 +1,12 @@
+package com.example.ppm04.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.UUID
+
+@Entity(tableName = "notes")
+data class Note(
+    @PrimaryKey val id: String = UUID.randomUUID().toString(),
+    val title: String,
+    val content: String
+)
